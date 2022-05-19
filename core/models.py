891d2ac31a0701fccbaef5389360449f339ed5d4
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-19 16:50:34 -0400

    Linking User model to Profile model w/ foreign key

--- core/models.py
+++ core/models.py
@@ -1,9 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
-  user = pass
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   id_user = pass
   bio = pass
-  profileimg = models.ImageField()
+  profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-image.jpeg')
   location = pass
