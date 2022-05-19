65af8fef3cce94e2eef9c91b96c31a3134dfcd0c
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-19 17:21:46 -0400

    error message if passwords do not match

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
@@ -15,6 +15,8 @@ def signup(request):
     if password == password2:
       pass
     else:
+      messages.info(request, 'Passwords do not match')
+      return redirect('signup')
 
 
   else:
