518e0417f18e0bcabfefa8589c6bbd8fde790e3d
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-19 17:25:32 -0400

    Writing signup logic for email and user taken

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import email
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
@@ -13,7 +14,14 @@ def signup(request):
     password2 = request.POST['password2']
 
     if password == password2:
-      pass
+      if User.objects.filter(email=email).exists():
+        messages.info(request, 'This email has already been used')
+        return redirect('signup')
+      elif User.objects.filter(username=username).exists():
+        messages.info(request, 'This username is taken')
+        return redirect('signup')
+      else:
+        User = User.objects.create_user(username=username, password=password)
     else:
       messages.info(request, 'Passwords do not match')
       return redirect('signup')
