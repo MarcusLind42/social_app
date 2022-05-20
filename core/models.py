ce9bc2936b9c7293ca00da5e351e95e9b13a3658
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-20 17:10:25 -0400

    Stubbing out Post model

--- core/models.py
+++ core/models.py
@@ -21,10 +21,13 @@ class Profile(models.Model):
 
 class Post(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4 )
-  user =
-  image = 
-  caption = 
-  created_at =
-  no_of_likes =
+  user = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='post_images')
+  caption = models.TextField()
+  created_at = models.DateTimeField(default=datetime.now)
+  no_of_likes = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.user
 
   
