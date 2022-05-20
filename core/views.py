e1c1d493cc4ceefd013c76ed3c3d5139be5b3ac9
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-20 15:35:15 -0400

    protecting index view with login required decorator

--- core/views.py
+++ core/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
   return render(request, 'index.html')
 
