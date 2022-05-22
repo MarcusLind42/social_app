642a8aff6700f7d0ad6e5516dea367299fec1cc2
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-22 14:25:23 -0400

    Deleting dead code

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
-
 ]
 
--- core/views.py
+++ core/views.py
@@ -72,8 +72,6 @@ def logout(request):
 @login_required(login_url='signin')
 def settings(request):
 
-
-
   user_profile = Profile.objects.get(user=request.user)
 
   if request.method == 'POST':
